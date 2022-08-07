d3e956b9a8c839f5a1ae2acd2b844cc1d79bdf14
chuanlingshi <shichuanling97@gmail.com>
2022-08-07 23:07:00 +0000

    implemented signin & signup

--- handler/router.go
+++ handler/router.go
@@ -1,14 +1,30 @@
 package handler
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/gorilla/mux"
+    jwtmiddleware "github.com/auth0/go-jwt-middleware"
+    jwt "github.com/form3tech-oss/jwt-go"
+    "github.com/gorilla/mux"    
 )
 
 func InitRouter() *mux.Router {
-	router := mux.NewRouter()
-	router.Handle("/upload", http.HandlerFunc(uploadHandler)).Methods("POST")
-	router.Handle("/search", http.HandlerFunc(searchHandler)).Methods("GET")
-	return router
+
+    jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+        ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+            return []byte(mySigningKey), nil
+        },
+        SigningMethod: jwt.SigningMethodHS256,
+    })
+
+    router := mux.NewRouter()
+
+    router.Handle("/upload", jwtMiddleware.Handler(http.HandlerFunc(uploadHandler))).Methods("POST")
+    router.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(searchHandler))).Methods("GET")
+
+    router.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+    router.Handle("/signin", http.HandlerFunc(signinHandler)).Methods("POST")
+
+    return router
 }
+
